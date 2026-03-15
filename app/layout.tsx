103708ee4e2603b3e6a970ca63fbe02790f46b8f
yukishafa <maheswari.yukishafa@gmail.com>
2026-03-15 13:31:18 +0700

    refactor: implement global layout container

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,11 @@ export default function RootLayout({
       >
         <FavoriteProvider>
           <Navbar />
-          {children}
+
+          <div className="max-w-6xl mx-auto px-4 md:px-6 lg:px-8">
+            {children}
+          </div>
+  
         </FavoriteProvider>
       </body>
     </html>
